f0debdbe4670895ac8ae36c5effce04f308bb93b
Sebastian Utoiu <sebiutoiu12@gmail.com>
2022-07-17 19:11:15 +0300

    Initial commit

---
+++ app/src/main/java/usebi/moviee/MainActivity.kt
@@ -0,0 +1,13 @@
+package usebi.moviee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
